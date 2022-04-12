06d95a26e04d4d9b4dfb59fadd2f3ad1377e38a1
Ashley <ashleynavo@googlemail.com>
2022-04-12 22:13:57 +0100

    app.test.js test updated and passed

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,22 @@
 import { cleanup, render } from "@testing-library/react";
 import App from "../components/App";
+import { unmountComponentAtNode } from "react-dom";
 
+let container = null
+
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
 
 afterEach(() => {
-  cleanup()
-})
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
 it("Should take a snapshot", () => {
   const {asFragment} = render(<App />);
