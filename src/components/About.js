556ba15d7ddd492f957d6deb7e722437bb0ccff8
Ashley <ashleynavo@googlemail.com>
2022-04-13 23:29:45 +0100

    Added description and test for description

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 export default function About() {
+
+  const description = "Placeholder description"
+
   return (
     <div>
         <h1>
             About the team
         </h1>
+        <div>
+          {description}
+        </div>
     </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function NavBar() {
 
-    const url = ""
-
   return (
     <div>
         <h1>SISIG FC</h1>
